1829470e50c8d519eb27635942fea606a935759e
glebk <glebkot57@gmail.com>
2023-12-16 23:45:33 +0200

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,20 @@
 FROM python:3.8
 
-# Установка зависимостей
-RUN apt-get update && apt-get install -y \
-    postgresql-client \
-    && rm -rf /var/lib/apt/lists/*
-
-# Устанавливаем рабочую директорию
 WORKDIR /app
 
-# Копируем файлы зависимостей
 COPY celery/requirements.txt /app/
-
-# Устанавливаем зависимости
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Копируем проект в образ
 COPY . /app/
 
-# Команда для запуска Django-приложения
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["sh", "-c", "python manage.py makemigrations && python manage.py migrate && python manage.py runserver 0.0.0.0:8000"]
+
+ENV DB_NAME=${DB_NAME}
+ENV DB_USER=${DB_USER}
+ENV DB_PASSWORD=${DB_PASSWORD}
+ENV DB_HOST=${DB_HOST}
+ENV DB_PORT=${DB_PORT}
+ENV DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE}
+ENV CELERY_BROKER=${CELERY_BROKER}
+ENV TELEGRAM_BOT_API_KEY=${TELEGRAM_BOT_API_KEY}
+ENV SECRET_KEY=${SECRET_KEY}
--- postgresql/init.sql
+++ postgresql/init.sql
@@ -1,11 +1,14 @@
 -- Создание базы данных
-CREATE DATABASE atom_bot_project;
+CREATE DATABASE ${DB_NAME};
 
 -- Создание пользователя и предоставление ему прав доступа
-CREATE USER postgres WITH PASSWORD '0052533';
-ALTER ROLE postgres SET client_encoding TO 'utf8';
-ALTER ROLE postgres SET default_transaction_isolation TO 'read committed';
-ALTER ROLE postgres SET timezone TO 'UTC';
+CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASSWORD}';
+ALTER ROLE ${DB_USER} SET client_encoding TO 'utf8';
+ALTER ROLE ${DB_USER} SET default_transaction_isolation TO 'read committed';
+ALTER ROLE ${DB_USER} SET timezone TO 'UTC';
 
 -- Предоставление прав доступа пользователю к базе данных
-GRANT ALL PRIVILEGES ON DATABASE atom_bot_project TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USER};
+
+# Копируем инициализационные SQL-скрипты в директорию, которая будет автоматически выполнена при запуске контейнера
+COPY postgresql/init.sql /docker-entrypoint-initdb.d/

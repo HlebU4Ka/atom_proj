7fc1990db58ef7b0817bcb0a0789e40f9ea929d0
glebk <glebkot57@gmail.com>
2023-12-19 09:38:34 +0200

    docker

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -1,11 +1,11 @@
 -- Создание базы данных
-CREATE DATABASE atom_bot_project;
+CREATE DATABASE ${POSTGRES_DB_NAME};
 
 -- Создание пользователя и предоставление ему прав доступа
-CREATE USER postgres WITH PASSWORD '0052533';
+CREATE USER postgres WITH PASSWORD ${DB_PASSWORD};
 ALTER ROLE postgres SET client_encoding TO 'utf8';
 ALTER ROLE postgres SET default_transaction_isolation TO 'read committed';
 ALTER ROLE postgres SET timezone TO 'UTC';
 
 -- Предоставление прав доступа пользователю к базе данных
-GRANT ALL PRIVILEGES ON DATABASE atom_bot_project TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB_NAME} TO ${POSTGRES_USER_NAME};
--- settings_app/settings.py
+++ settings_app/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = 'settings_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
+        'NAME': os.getenv('POSTGRES_DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER_NAME'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST': os.getenv('POSTGRES_HOST_NAME'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_API_KEY')
